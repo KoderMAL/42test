5bf5d264c3391bf32ce9ebb92ab5edcb8c2c97b5
Amine LALAOUI <alalaoui@student.42.fr>
2017-09-11 17:26:08 +0200

    correc atoll

--- ft_atoll.c
+++ ft_atoll.c
@@ -6,11 +6,17 @@
 /*   By: alalaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/09/08 13:58:45 by alalaoui          #+#    #+#             */
-/*   Updated: 2017/09/08 14:02:43 by alalaoui         ###   ########.fr       */
+/*   Updated: 2017/09/11 16:35:13 by alalaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
+
+static int			ft_isspace(int c)
+{
+	return (c == '\t' || c == '\n' || c == '\r' || c == '\v' || c == '\f'
+			|| c == ' ');
+}
 
 long long			ft_atoll(const char *str)
 {
