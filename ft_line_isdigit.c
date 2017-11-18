d951a0a1b08f9c10bd3cff311278267e4b821424
Amine LALAOUI <alalaoui@student.42.fr>
2017-11-18 18:13:52 +0100

    add condition on first negative sign

--- ft_line_isdigit.c
+++ ft_line_isdigit.c
@@ -6,7 +6,7 @@
 /*   By: alalaoui <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/10/10 18:05:31 by alalaoui          #+#    #+#             */
-/*   Updated: 2017/10/17 17:25:33 by alalaoui         ###   ########.fr       */
+/*   Updated: 2017/10/21 16:31:33 by alalaoui         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,8 @@ int			ft_line_isdigit(char *line)
 {
 	if (!*line)
 		return (0);
+	if (*line == '-')
+		line++;;
 	while (*line)
 	{
 		if (!ft_isdigit(*line))
